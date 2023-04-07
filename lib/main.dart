272534e56c18d63b39ab36c7e7d8c79782b0c2f4
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-04-07 18:24:55 +0000

    precache image files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:e_magazine_ui_challenge/src/ui/ui.dart';
+import 'package:e_magazine_ui_challenge/src/utils/constants/asset_consts.dart';
 import 'package:e_magazine_ui_challenge/src/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'src/blocs/blocs.dart';
 
 void main() {
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  binding.addPostFrameCallback((_) async {
+    BuildContext context = binding.renderViewElement!;
+    for (var asset in AssetConsts.allAssets()) {
+      precacheImage(AssetImage(asset), context);
+    }
+  });
   runApp(const MainApp());
 }
 
@@ -11,10 +22,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.defaultTheme,
-      home: const MagazineHomeScreen(),
+    return BlocProvider<MagazineCardCubit>(
+      create: (context) => MagazineCardCubit(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.defaultTheme,
+        home: const MagazineHomeScreen(),
+      ),
     );
   }
 }
