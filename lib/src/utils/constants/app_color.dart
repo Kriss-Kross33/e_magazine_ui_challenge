c119d9c23fb488131dec8555c2664fa80d0fc519
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-04-07 18:27:45 +0000

    create  helper class to store color codes

---
+++ lib/src/utils/constants/app_color.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color alabaster = Color(0xFFEDEAE0);
+  static const Color black = Color(0xFF070d09);
+  static const Color gold = Color(0xFFFEBE00);
+  static const Color green = Color(0xFF8EC919);
+  static const Color grey = Color(0xFFD6D6D7);
+  static const Color cyan = Colors.cyan;
+  static const Color red = Colors.redAccent;
+  static const Color violet = Color(0xFF19192E);
+  static const Color darkGreen = Color(0xFF484B39);
+  static const Color lemonGreen = Color(0xFF416b4A);
+  static const Color charcoalBlack = Color(0xFF252525);
+
+  // static const Color gold = Color(0xFFFEBE00);
+  // static const Color gold = Color(0xFFFEBE00);
+}
