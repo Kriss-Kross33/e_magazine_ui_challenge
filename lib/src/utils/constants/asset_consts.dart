dd8a3d9f030855ee15bce24c417bb4d3c17b2bbd
Kwame Adusei Fosu <kwamefosu33@gmail.com>
2023-04-07 18:28:30 +0000

    create  helper class to store image asset path

---
+++ lib/src/utils/constants/asset_consts.dart
@@ -0,0 +1,25 @@
+class AssetConsts {
+  AssetConsts._();
+
+  static const String scanner = 'assets/images/scanner.png';
+  static const String search = 'assets/images/search.png';
+  static const String dazed1 = 'assets/images/dazed_1.jpeg';
+  static const String dazed2 = 'assets/images/dazed_2.jpeg';
+  static const String dazed3 = 'assets/images/dazed_3.jpeg';
+  static const String dazed4 = 'assets/images/dazed_4.jpeg';
+  static const String dazed5 = 'assets/images/dazed_5.jpeg';
+  static const String magItem1 = 'assets/images/mag_item1.jpeg';
+  static const String magItem2 = 'assets/images/mag_item2.jpeg';
+  static const String magItem3 = 'assets/images/mag_item3.jpeg';
+  static const String magItem4 = 'assets/images/mag_item4.jpeg';
+
+  static List<String> allAssets() {
+    return <String>[
+      dazed1,
+      dazed2,
+      dazed3,
+      dazed4,
+      dazed5,
+    ];
+  }
+}
